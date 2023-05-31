2ee731cf617be0893c47b14621a9591ed727ccd7
Leonid Deev <ltupov2003@yandex.ru>
2023-05-31 15:09:40 +0300

    Create Fragment Search

--- app/src/main/java/com/example/nexusdev/MainActivity.java
+++ app/src/main/java/com/example/nexusdev/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.nexusdev;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,8 +13,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Fragment fragment = new MainFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.startFragment, fragment);
+        fragmentTransaction.commit();
     }
 
     public void MainButton(View view)
--- app/src/main/java/com/example/nexusdev/StorageActivity.java
+++ app/src/main/java/com/example/nexusdev/StorageActivity.java
@@ -10,6 +10,7 @@ public class StorageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_storage);
     }
